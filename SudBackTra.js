e7ddc168acf7669a2b64f357226d2b902a25b30a
arksgproz <106900279+arksgproz@users.noreply.github.com>
2022-06-05 11:43:51 +0530

    Update SudBackTra.js

--- SudBackTra.js
+++ SudBackTra.js
@@ -1,1 +1,14 @@
-
+function InsertDat(){
+  var tabl=document.getElementByClassName("DispSud");
+  var tbod=document.createElement("tbody");
+  
+  for(var i=0;i<10;i++){
+    var trow=documrnt.createElement("tr");
+    for(var j=0;j<10;j++){
+      var tdata=document.createElement("td");
+      var dat=document.createTextNode(j);
+      tdata.appendChild(dat);
+    }
+    trow.appendChild(tdata);
+  }
+}
